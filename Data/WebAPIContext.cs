b88eb88afd56ed0769a8d7f3b1693081fc69d06f
Cristina Constantin <constantin.cristina2002@gmail.com>
2024-12-18 19:53:36 +0200

    Add project files.

---
+++ Data/WebAPIContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ConstantinCristinaLab11.Models;
+
+namespace ConstantinCristinaLab11.Data
+{
+    public class WebAPIContext : DbContext
+    {
+        public WebAPIContext (DbContextOptions<WebAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ConstantinCristinaLab11.Models.ShopList> ShopList { get; set; } = default!;
+    }
+}
